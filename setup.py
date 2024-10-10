6fc71b77deba8f94361a2a8e75751600cb53eeb1
Nora Zhao <ziying.zhao@autuni.ac.nz>
2024-10-11 12:55:19 +1300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+#    This is a brief tutorial of files to install Geatpy
+#
+#    Geatpy is a free toolbox: you can redistribute it and/or modify as you want.
+#
+#    Geatpy is distributed in the hope that it will be useful for the genetic
+#    and evolutionary algorithm, you can get the tutorial from http://geatpy.github.io
+#
+#    If you want to donate to it, please email geatpy@163.com
+#
+#    Installation way:
+#    pip install geatpy
